e032d4e37e395d1c4b0c19ee43d3397e4f7ce900
Rajbir Sharma <rajkaushik05@gmail.com>
2019-12-12 20:11:17 +0530

    update build code

---
+++ build/precache-manifest.bc1917216fe88fd0e212630d776b3be0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45ede86c8944d8257e7d565d5bf6c1d5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4141a10538d3b3ea562d",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "2dac7abf191bbe8175ad",
+    "url": "/static/css/main.b8765e77.chunk.css"
+  },
+  {
+    "revision": "4141a10538d3b3ea562d",
+    "url": "/static/js/2.b4383ee5.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.b4383ee5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2dac7abf191bbe8175ad",
+    "url": "/static/js/main.4688dec0.chunk.js"
+  },
+  {
+    "revision": "a2076e2b6ff597e10196",
+    "url": "/static/js/runtime-main.30cba478.js"
+  }
+]);
